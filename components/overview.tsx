14681f1db24d2e77812ee7be70ce7c39ec28a56b
olatemitayo <olatunjitemitayo444@gmail.com>
2023-08-07 13:15:24 +0100

    updates

---
+++ components/overview.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Data = [
+  {
+    id: "1",
+    element: "Wind Speed",
+    volume: "12km/h",
+    img: "/wind.svg",
+  },
+  {
+    id: "2",
+    element: "Rain chance",
+    volume: "24%",
+    img: "/wind.svg",
+  },
+  {
+    id: "3",
+    element: "Pressure",
+    volume: "720 hpa",
+    img: "/wave-line.svg",
+  },
+  {
+    id: "4",
+    element: "Uv Index",
+    volume: "2,3",
+    img: "/sun.svg",
+  },
+];
+export default function Overview() {
+  return (
+    <>
+      <div>
+        <div className="">
+          <h1 className="mb-4">Today's overview</h1>
+          <div className="flex flex-wrap items-center justify-between gap-3 ">
+            {Data.map((item) => (
+              <div
+                key={item.id}
+                className="bg-[#f0eeee] min-w-[96.06px] p-[clamp(5px,2vw,20px)] rounded-lg flex gap-4 items-center justify-center "
+              >
+                <img
+                  src={item.img}
+                  alt={item.element}
+                  className="w-[clamp(12px,2vw,24px)]"
+                />
+                <div className="">
+                  <p className="text-[clamp(10px,1vw,16px)]">{item.element}</p>
+                  <h1 className="text-[clamp(16px,1vw,24px)] ">
+                    {item.volume}
+                  </h1>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/weather.tsx
+++ components/weather.tsx
@@ -2,6 +2,7 @@ import { UserDetails } from "@/pages/_app";
 import React, { useEffect, useState } from "react";
 import Clock from "./current-time";
 import Rain from "./rain";
+import Overview from "./overview";
 
 export default function Weather() {
   const [title, setTitle] = useState<UserDetails>(null);
@@ -15,9 +16,24 @@ export default function Weather() {
   }, []);
   return (
     <main className="">
-      <div className="mx-auto  flex cmd:flex-col-reverse justify-between items-center px-[clamp(20px,3vw,40px)] py-[clamp(10px,2vw,16px)] max-w-[1500px]  ">
-        <div className="w-[70%] cmd:w-[100%] max-h-[100%]    px-[clamp(20px,3vw,35px)] py-[clamp(10px,2vw,16px)] ">
-          hiii
+      <div className="mx-auto  flex cmd:flex-col-reverse justify-between items-center px-[clamp(20px,3vw,40px)] py-[clamp(10px,2vw,16px)] max-w-[1500px] gap-4 ">
+        <div className="w-[70%] cmd:w-[100%] max-h-[100%]    px-[clamp(20px,3vw,35px)] py-[clamp(10px,2vw,16px)] border border-[#f0eeee] rounded-2xl">
+          <div className="">
+            <Overview />
+          </div>
+          <div className="mt-4">
+            <img src="/sunny.svg" alt="" className="w-[40px]" />
+            <div className="flex items-center justify-between">
+              <h1 className="text-[48px]">20°C</h1>
+              <div>
+                <p>Dramatic</p>
+                <p>Cloudy</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <Rain />
+          </div>
         </div>
 
         <div className="flex-1 cmd:w-[100%] max-h-[100%]  rounded-2xl bg-[#172658]  px-[clamp(20px,3vw,35px)] py-[clamp(10px,2vw,20px)] text-[#fff]">
